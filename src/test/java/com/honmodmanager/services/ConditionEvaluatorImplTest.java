0b74379a2c09e2c05ffb8ea7a46296440c0c448c
Burgy Benjamin <minidfx@gmail.com>
2015-09-28 12:53:03 +0200

    Fix unit tests but not complete.

--- src/test/java/com/honmodmanager/services/ConditionEvaluatorImplTest.java
+++ src/test/java/com/honmodmanager/services/ConditionEvaluatorImplTest.java
@@ -2,12 +2,16 @@ package com.honmodmanager.services;
 
 import com.github.jlinqer.collections.List;
 import com.github.jlinqer.linq.IEnumerable;
+import com.honmodmanager.models.ConditionElement;
 import com.honmodmanager.models.ModImpl;
 import com.honmodmanager.models.VersionImpl;
 import com.honmodmanager.models.contracts.Mod;
 import com.honmodmanager.models.contracts.Version;
 import junit.framework.TestCase;
 import org.junit.Assert;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -40,10 +44,12 @@ public final class ConditionEvaluatorImplTest extends TestCase
     {
         Version versionStub = new VersionImpl();
         Mod modStub1 = new ModImpl("stub1", versionStub, true);
+        ConditionElement conditionMock = mock(ConditionElement.class);
+        when(conditionMock.getCondition()).thenReturn("");
 
         IEnumerable<Mod> mods = new List<>(modStub1);
 
-        boolean result = this.instance.evaluate(modStub1.getEditElements().first(), mods);
+        boolean result = this.instance.evaluate(conditionMock, mods);
 
         Assert.assertTrue(result);
     }
@@ -52,10 +58,12 @@ public final class ConditionEvaluatorImplTest extends TestCase
     {
         Version versionStub = new VersionImpl();
         Mod modStub1 = new ModImpl("stub1", versionStub, true);
+        ConditionElement conditionMock = mock(ConditionElement.class);
+        when(conditionMock.getCondition()).thenReturn("mod1");
 
         IEnumerable<Mod> mods = new List<>(modStub1);
 
-        boolean result = this.instance.evaluate(modStub1.getEditElements().first(), mods);
+        boolean result = this.instance.evaluate(conditionMock, mods);
 
         Assert.assertTrue(result);
     }
